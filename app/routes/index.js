32ed2cd77364394e84fbcd88818bbc6f9b359c9e
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-11-17 20:02:45 -0300

    restore values

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const allowCors = require('../middlewares/allowCors')
 
 const usersRouter = require('./users')
 const authRouter = require('./auth')
@@ -8,9 +7,9 @@ const visitsRouter = require('./visits')
 const observedValuesRouter = require('./observedValues')
 
 
-router.use('/', allowCors, usersRouter);
-router.use('/', allowCors, authRouter);
-router.use('/', allowCors, visitsRouter);
-router.use('/', allowCors, observedValuesRouter);
+router.use('/', usersRouter);
+router.use('/', authRouter);
+router.use('/', visitsRouter);
+router.use('/', observedValuesRouter);
 
 module.exports = router;
