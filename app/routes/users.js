d0ecd39732ac2751ebf7979c724e4d502e291c14
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-11-17 20:12:28 -0300

    quit auth from users route

--- app/routes/users.js
+++ app/routes/users.js
@@ -7,6 +7,6 @@ const userExist = require('../middlewares/userExist');
 const validatePassword = require('../middlewares/validPasswordBeforeHashing')
 
 module.exports = router.get('/users', auth, UserController.getAllUsers)
-    .post('/users', auth, validatePassword, UserController.createUser)
+    .post('/users', validatePassword, UserController.createUser)
     .put('/users/:id', auth, userExist, validatePassword, UserController.updateUser)
     .delete('/users/:id', auth, userExist, UserController.deleteUser);
