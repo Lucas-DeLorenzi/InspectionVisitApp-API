af4577989287ce8bdc7a04fb45f1a0c3df8a32c3
Lucas De Lorenzi <lucasrauldelorenzi@gmail.com>
2022-11-23 17:18:11 -0300

    add team route to index routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,11 +5,13 @@ const usersRouter = require('./users')
 const authRouter = require('./auth')
 const visitsRouter = require('./visits')
 const observedValuesRouter = require('./observedValues')
+const teamsRouter = require('./teams')
 
 
 router.use('/', usersRouter);
 router.use('/', authRouter);
 router.use('/', visitsRouter);
 router.use('/', observedValuesRouter);
+router.use('/', teamsRouter);
 
 module.exports = router;
